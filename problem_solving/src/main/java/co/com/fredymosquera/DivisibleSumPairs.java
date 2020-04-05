95aa5cf225fa36da3da434e91d482029117cc60a
Fredy Mosquera <fmosquera0101@gmail.com>
2020-04-05 00:06:28 -0500

    Solving Divisible sum pairs

---
+++ problem_solving/src/main/java/co/com/fredymosquera/DivisibleSumPairs.java
@@ -0,0 +1,30 @@
+package co.com.fredymosquera;
+
+public class DivisibleSumPairs {
+
+    public static void main(String[] args) {
+
+        int[] ar = new int[]{1, 3, 2, 6, 1, 2};
+        int n = 6;
+        int k = 3;
+        System.out.println(divisibleSumPairs(n, k, ar));
+    }
+
+    // Complete the divisibleSumPairs function below.
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int pairs = 0;
+
+        for (int i = 0; i < n - 1 ; i++) {
+            int sum;
+            for (int j = i+1; j < n  ; j++) {
+                sum = ar[i] + ar[j];
+                if(sum % k == 0){
+                    pairs++;
+                }
+            }
+        }
+
+        return  pairs;
+
+    }
+}
---
+++ problem_solving/src/main/java/co/com/fredymosquera/DivisibleSumPairsWithStreams.java
@@ -0,0 +1,28 @@
+package co.com.fredymosquera;
+
+import java.util.stream.IntStream;
+
+public class DivisibleSumPairsWithStreams {
+
+    public static void main(String[] args) {
+
+        int[] ar = new int[]{1, 3, 2, 6, 1, 2};
+        int n = 6;
+        int k = 3;
+
+        System.out.println(divisibleSumPairs(n, k, ar));
+    }
+
+    // Complete the divisibleSumPairs function below.
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        long pairs =IntStream.rangeClosed(0, n-1)
+                .flatMap(
+                        i -> IntStream.rangeClosed(i+1, n-1)
+                        .map(j -> ar[i] + ar[j])
+                        .filter(l -> l % k == 0)
+                ).count();
+
+        return (int) pairs;
+
+    }
+}
