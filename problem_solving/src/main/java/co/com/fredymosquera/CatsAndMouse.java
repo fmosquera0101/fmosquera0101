4e7e50ea0159827269517a57bfd8736c4e4fb5d7
Fredy Mosquera <fmosquera0101@gmail.com>
2020-04-08 21:56:25 -0500

    refactoring cats and mouse proble

--- problem_solving/src/main/java/co/com/fredymosquera/CatsAndMouse.java
+++ problem_solving/src/main/java/co/com/fredymosquera/CatsAndMouse.java
@@ -1,4 +1,22 @@
 package co.com.fredymosquera;
 
 public class CatsAndMouse {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 2;
+        int z = 2;
+        System.out.println(catAndMouse(x, y, z));
+    }
+    // Complete the catAndMouse function below.
+    static String catAndMouse(int x, int y, int z) {
+        if(Math.abs(x - z) < Math.abs(y-z)){
+            return "Cat A";
+        }
+        else if (Math.abs(x - z) > Math.abs(y-z)){
+            return "Cat B";
+        }
+        else{
+            return "Mouse C";
+        }
+    }
 }
