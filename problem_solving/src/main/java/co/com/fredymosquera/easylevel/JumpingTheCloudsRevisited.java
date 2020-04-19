ad135431411a489cf32d321340f2f42552b1ab07
Fredy Mosquera <fmosquera0101@gmail.com>
2020-04-18 22:29:22 -0500

    Solving jumping gthe clouds, optimizing algorithm

--- problem_solving/src/main/java/co/com/fredymosquera/easylevel/JumpingTheCloudsRevisited.java
+++ problem_solving/src/main/java/co/com/fredymosquera/easylevel/JumpingTheCloudsRevisited.java
@@ -3,34 +3,21 @@ package co.com.fredymosquera.easylevel;
 public class JumpingTheCloudsRevisited {
 
     public static void main(String[] args) {
-        int[] c = {1 ,1 ,1, 0, 1, 1, 0, 0, 0, 0};
+        int[] c = {1, 1, 1, 0, 1, 1, 0, 0, 0, 0};
         int k = 3;
+
         System.out.println(jumpingOnClouds(c, k));
     }
 
     static int jumpingOnClouds(int[] c, int k) {
 
         int energy = 100;
-        int i =  k;
-        boolean end = false;
-        while (!end){
-            if(i >= c.length - 1){
-                i = 0;
-                if(c[i] == 0){
-                    energy--;
-                    end = true;
-                }else {
-                    energy -= 3;
-                }
-
-            }else {
-                if(c[i] == 0){
-                    energy--;
-                }else {
-                    energy -= 3;
-                }
-                i += k;
-            }
+        int n = c.length;
+        int i = k % n;
+        energy -= c[i] * 2 + 1;
+        while (i != 0) {
+            i = (i + k) % n;
+            energy -= c[i] * 2 + 1;
 
         }
         return energy;
