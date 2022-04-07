edf49e6d5db679396294d692d1a44715826547c7
Fredy Mosquera <fmosquera@Macbooks-MacBook-Pro.local>
2022-04-07 18:58:29 -0500

    Solving ACM ICPC problem

---
+++ problem_solving/src/main/java/co/com/fredymosquera/easylevel/ACMICPCTeam.java
@@ -0,0 +1,94 @@
+package co.com.fredymosquera.easylevel;
+
+import java.util.*;
+
+public class ACMICPCTeam {
+
+    public static void main(String[] args) {
+        List<String> topic = Arrays.asList(
+                "10101",
+                "11110",
+                "00010");
+        System.out.println(acmTeam(topic));
+    }
+
+    public static List<Integer> acmTeam(List<String> topic) {
+        // Write your code here
+
+        Map<String, Integer> teamsTopics = new HashMap<>();
+        Map<String, String> teamPairs = new HashMap<>();
+        Set<Integer> numTeamsMax = new HashSet<>();
+        int max = 0;
+        for (int i = 0; i < topic.size() ; i++) {
+            for (int j = i + 1; j < topic.size(); j++) {
+                String teamName = topic.get(i) +" - "+topic.get(j);
+                teamPairs.put(teamName, topic.get(i) +""+topic.get(j));
+//                String[] knownTopicsA = topic.get(i).split("");
+//                String[] knownTopicsB = topic.get(j).split("");
+//
+//                for (int k = 0; k <  knownTopicsA.length; k++) {
+//                    if(knownTopicsA[k].equals("1") || knownTopicsB[k].equals("1")){
+//                        teamsTopics.put(teamName,teamsTopics.getOrDefault( teamName, 0) + 1);
+//                        if(max < teamsTopics.get(teamName)){
+//                            max = teamsTopics.get(teamName);
+//                        }
+//                    }
+//                }
+
+
+
+            }
+        }
+        //0 1 2 3 4 5 6 7
+        for(Map.Entry<String, String> entry : teamPairs.entrySet()){
+            String[] knowTopics = entry.getValue().split("");
+            for (int i = 0, j = knowTopics.length/ 2; i < knowTopics.length/2 ; i++, j--) {
+                if ("1".equals(knowTopics[i]) || "1".equals(knowTopics[j])){
+                    teamsTopics.put(entry.getKey(), teamsTopics.getOrDefault( entry.getKey(), 0) + 1);
+                        if(max < teamsTopics.get(entry.getKey())){
+                            max = teamsTopics.get(entry.getKey());
+                        }
+                }
+            }
+
+        }
+        int finalMax = max;
+        int teams = (int) teamsTopics.values().stream().filter(v -> v == finalMax).count();
+
+        return Arrays.asList(max, teams);
+    }
+
+    public static List<Integer> acmTeamBruteForce(List<String> topic) {
+        // Write your code here
+
+        Map<String, Integer> teamsTopics = new HashMap<>();
+        Map<String, String> teamPairs = new HashMap<>();
+        Set<Integer> numTeamsMax = new HashSet<>();
+        int max = 0;
+        for (int i = 0; i < topic.size() ; i++) {
+            for (int j = i + 1; j < topic.size(); j++) {
+                String teamName = topic.get(i) +" - "+topic.get(j);
+                teamPairs.put(teamName, topic.get(i) +""+topic.get(j));
+                String[] knownTopicsA = topic.get(i).split("");
+                String[] knownTopicsB = topic.get(j).split("");
+
+                for (int k = 0; k <  knownTopicsA.length; k++) {
+                    if(knownTopicsA[k].equals("1") || knownTopicsB[k].equals("1")){
+                        teamsTopics.put(teamName,teamsTopics.getOrDefault( teamName, 0) + 1);
+                        if(max < teamsTopics.get(teamName)){
+                            max = teamsTopics.get(teamName);
+                        }
+                    }
+                }
+
+
+
+            }
+        }
+
+        int finalMax = max;
+        int teams = (int) teamsTopics.values().stream().filter(v -> v == finalMax).count();
+
+        return Arrays.asList(max, teams);
+    }
+}
