cd2d5a1f7bd0b80ea7ec202453facc32ec151e0e
Fredy Mosquera <fmosquera0101@gmail.com>
2022-05-06 22:06:39 -0500

    Beautiful triplets

--- problem_solving/src/main/java/co/com/fredymosquera/easylevel/BeautifulTriplets.java
+++ problem_solving/src/main/java/co/com/fredymosquera/easylevel/BeautifulTriplets.java
@@ -1,6 +1,6 @@
 package co.com.fredymosquera.easylevel;
 
-import java.util.List;
+import java.util.*;
 
 public class BeautifulTriplets {
 
@@ -12,26 +12,32 @@ public class BeautifulTriplets {
         // Write your code here
 
         int count = 0;
-        for (int i = 0; i < arr.size() -2; i++) {
-            for (int j = i + 1; j < arr.size() -1; j++) {
-                if (arr.get(j) > arr.get(i)){
-                    for (int k = j + 1; k < arr.size(); k++) {
-                        if (arr.get(k) > arr.get(j)){
-                            int subs1 = arr.get(j) - arr.get(i);
-                            int subs2 = arr.get(k) - arr.get(j);
-
-                            if(subs1 == subs2 && subs1 == d){
-                                count++;
-                            }
-                        }else {
-                            continue;
-                        }
-                    }
-                }else{
-                    continue;
-                }
+        for (Integer value : arr) {
+            if(Collections.binarySearch(arr, value + d) >= 0 && Collections.binarySearch(arr, value + d * 2) >= 0){
+                count++;
+            }
+        }
 
 
+        return count;
+
+    }
+
+    public static int beautifulTripletsSets(int d, List<Integer> arr) {
+        // Write your code here
+
+        int count = 0;
+        Set<Integer> set = new HashSet<>();
+        for (Integer value : arr) {
+            if(!set.contains(value)){
+                set.add(value);
+            }
+        }
+
+
+        for (Integer value : arr) {
+            if(set.contains(value + d) && set.contains(value + d * 2)){
+                count++;
             }
         }
 
